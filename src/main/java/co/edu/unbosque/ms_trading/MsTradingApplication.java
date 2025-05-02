8c014b9aa1160ce4fef62a2c9043f2d6b189bd93
sebastian cardenas <scardenasga@unbosque.edu.co>
2025-05-02 17:37:02 -0500

    Primer Commit del microservicio Trading ms-trading

---
+++ src/main/java/co/edu/unbosque/ms_trading/MsTradingApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ms_trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/ms_trading/MsTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ms_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
